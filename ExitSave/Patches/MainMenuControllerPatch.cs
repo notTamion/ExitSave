e99e51df9541c5da78ed61f63aabf1eff1654adc
Tamion <70228790+notTamion@users.noreply.github.com>
2024-12-29 19:14:40 +0100

    init

---
+++ ExitSave/Patches/MainMenuControllerPatch.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace TestingMod.Patches;
+
+[HarmonyPatch(typeof(MainMenuController))]
+public class MainMenuControllerPatch{
+    private static bool isExiting;
+    
+    [HarmonyPatch("ExitGame")]
+    [HarmonyPrefix]
+    static bool PatchExitGame(){
+        ButtonController saveGameButton = MainMenuController.Instance.saveGameButton;
+        if (isExiting){
+            return true;
+        }
+
+        isExiting = true;
+        PointerEventData data = new PointerEventData(null);
+        saveGameButton.OnSelect();
+        saveGameButton.OnPointerUp(data);
+        saveGameButton.OnPointerClick(data);
+        return false;
+    }
+}
---
+++ ExitSave/Plugin.cs
@@ -0,0 +1,20 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+using TestingMod.Patches;
+
+namespace ExitSave;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BasePlugin{
+    private const string GUID = "tamion.TestingModj";
+    private const string Name = "Testing Mod";
+    private const string Version = "1.0.0";
+
+    private readonly Harmony harmony = new Harmony(GUID);
+    
+    public override void Load(){
+        harmony.PatchAll(typeof(MainMenuControllerPatch));
+    }
+}
